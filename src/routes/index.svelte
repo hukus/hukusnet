933919ed6e917203a8233d486f54953814916bb4
Peter Hucik <peter.hucik@vacuumlabs.com>
2020-12-02 21:53:17 +0100

    fix(Title): hukusnet welcome

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -40,11 +40,11 @@
 	<title>Sapper project template</title>
 </svelte:head>
 
-<h1>Great success!</h1>
+<h1>hukusnet lives again!</h1>
 
 <figure>
 	<img alt="Success Kid" src="{successkid}">
 	<figcaption>Have fun with Sapper!</figcaption>
 </figure>
 
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+
