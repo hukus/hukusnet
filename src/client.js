223b06823236209d4c7a6c1f172b81dc1f0037d2
Peter Hucik <peter.hucik@vacuumlabs.com>
2020-12-03 15:05:04 +0100

    chore: json loading support in rollup

--- src/client.js
+++ src/client.js
@@ -1,7 +1,7 @@
 import * as sapper from '@sapper/app';
 // import Amplify from 'aws-amplify';
 // import awsconfig from './aws-exports';
-// Amplify.configure(awsconfig);
+// Amplify.configure();
 
 sapper.start({
 	target: document.querySelector('#sapper')
