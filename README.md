edd7425ee125cdd66fb53578a7639f066869ca84
Peter Hucik <peter.hucik@vacuumlabs.com>
2020-12-03 14:03:38 +0100

    chore: add prettier and cleanup files

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # hukus.net
+
 Learning Sapper and automatic deployments while making use of my web-hosting.
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,50 +1,49 @@
-<script>
-	import successkid from 'images/successkid.jpg';
-</script>
-
 <style>
-	h1, figure, p {
-		text-align: center;
-		margin: 0 auto;
-	}
+  h1,
+  figure,
+  p {
+    text-align: center;
+    margin: 0 auto;
+  }
 
-	h1 {
-		font-size: 2.8em;
-		text-transform: uppercase;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
+  h1 {
+    font-size: 2.8em;
+    text-transform: uppercase;
+    font-weight: 700;
+    margin: 0 0 0.5em 0;
+  }
 
-	figure {
-		margin: 0 0 1em 0;
-	}
+  figure {
+    margin: 0 0 1em 0;
+  }
 
-	img {
-		width: 100%;
-		max-width: 400px;
-		margin: 0 0 1em 0;
-	}
+  img {
+    width: 100%;
+    max-width: 400px;
+    margin: 0 0 1em 0;
+  }
 
-	p {
-		margin: 1em auto;
-	}
+  p {
+    margin: 1em auto;
+  }
 
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
+  @media (min-width: 480px) {
+    h1 {
+      font-size: 4em;
+    }
+  }
 </style>
 
+<script>
+  import successkid from "images/successkid.jpg";
+</script>
+
 <svelte:head>
-	<title>hukusnet</title>
+  <title>hukusnet</title>
 </svelte:head>
 
 <h1>hukusnet lives again!</h1>
 
-<figure>
-	<img alt="Success Kid" src="{successkid}">
-	<figcaption>Have fun with Sapper!</figcaption>
-</figure>
+<figure><img alt="Success Kid" src={successkid} /></figure>
 
-
+<p>And now let's add AWS Amplify support...</p>
