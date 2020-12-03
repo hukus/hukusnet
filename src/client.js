53d43a160c609c9813a1721c2178f82921d353a1
Peter Hucik <peter.hucik@vacuumlabs.com>
2020-12-03 14:43:42 +0100

    feat(AWS): add amplify auth

--- src/client.js
+++ src/client.js
@@ -1,4 +1,7 @@
 import * as sapper from '@sapper/app';
+// import Amplify from 'aws-amplify';
+// import awsconfig from './aws-exports';
+// Amplify.configure(awsconfig);
 
 sapper.start({
 	target: document.querySelector('#sapper')
